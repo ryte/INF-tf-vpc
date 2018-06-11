0d85ef18c193f2f099117ea4ce2817c39132f5b3
Christian Hunter <christian.hunter@kreuzwerker.de>
2018-06-11 15:20:12 +0200

    Include extra variables and resources needed

--- peering/main.tf
+++ peering/main.tf
@@ -8,6 +8,15 @@ resource "aws_vpc_peering_connection" "peer" {
   tags          = "${merge(local.tags,map("PeerSide","Requester"))}"
 }
 
+# Requester's side of the routing through the vpc peering.
+resource "aws_route" "peering_route_requester" {
+  depends_on                = ["aws_vpc_peering_connection.peer"]
+  count                     = "${length(var.requester_route_table_ids)}"
+  requester_route_table_id  = "${var.requester_route_table_ids[count.index]}"
+  destination_cidr_block    = "${var.accepter_cidr_v4}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
+}
+
 # Accepter's side of the connection. This resource will be created under the account that receives the VPC peering request.
 resource "aws_vpc_peering_connection_accepter" "peer" {
   depends_on                = ["aws_vpc_peering_connection.peer"]
@@ -15,4 +24,14 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
   auto_accept               = true
   tags                      = "${merge(local.tags,map("PeerSide","Accepter"))}"
+}
+
+# Accepter's side of the routing through the vpc peering.
+resource "aws_route" "peering_route_accepter" {
+  provider                  = "aws.peer"
+  depends_on                = ["aws_vpc_peering_connection_accepter.peer"]
+  count                     = "${length(var.route_table_ids)}"
+  accepter_route_table_id   = "${var.accepter_route_table_ids[count.index]}"
+  destination_cidr_block    = "${var.requester_cidr_v4}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection_accepter.peer.id}"
 }
--- peering/variables.tf
+++ peering/variables.tf
@@ -7,3 +7,7 @@ variable "main_vpc_id" {}
 variable "peer_vpc_id" {}
 variable "peer_account_id" {}
 variable "peer_region" {}
+variable "accepter_cidr_v4" {}
+variable "requester_cidr_v4" {}
+variable "requester_route_table_ids" {}
+variable "accepter_route_table_ids" {}
