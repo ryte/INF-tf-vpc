165d683340252eb8200c1f5fdd3b1a13103b6fe8
Christian Hunter <christian.hunter@kreuzwerker.de>
2018-06-11 12:31:06 +0200

    Adjust vpc peering resource creation; create outputs.tf to get the peering connection ID

--- peering/main.tf
+++ peering/main.tf
@@ -1,4 +1,4 @@
-# Requester's side of the connection.
+# Requester's side of the connection. This resource will be created under the account from which the VPC peering was requested.
 resource "aws_vpc_peering_connection" "peer" {
   vpc_id        = "${var.main_vpc_id}"
   peer_vpc_id   = "${var.peer_vpc_id}"
@@ -8,9 +8,10 @@ resource "aws_vpc_peering_connection" "peer" {
   tags          = "${merge(local.tags,map("PeerSide","Requester"))}"
 }
 
-# Accepter's side of the connection.
+# Accepter's side of the connection. This resource will be created under the account that receives the VPC peering request.
 resource "aws_vpc_peering_connection_accepter" "peer" {
   depends_on                = ["aws_vpc_peering_connection.peer"]
+  provider                  = "aws.peer"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.peer.id}"
   auto_accept               = true
   tags                      = "${merge(local.tags,map("PeerSide","Accepter"))}"
